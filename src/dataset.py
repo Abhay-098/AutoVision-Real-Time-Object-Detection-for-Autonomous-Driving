c1d7a49b7ed70d1a81b1f359171b58ff56bcd122
Abhay Kumar <145521876+Abhay-098@users.noreply.github.com>
2025-11-08 22:58:03 +0530

    Update dataset.py

--- src/dataset.py
+++ src/dataset.py
@@ -1,60 +1,68 @@
 import os
 import torch
+from torch.utils.data import Dataset
 from PIL import Image
-import xml.etree.ElementTree as ET
+import json
+from torchvision import transforms
 
-class VOCDataset(torch.utils.data.Dataset):
-    """A minimal Pascal VOC-style dataset wrapper.
-    Expects folders:
-      - JPEGImages/     (images)
-      - Annotations/    (VOC XMLs)
-      - ImageSets/Main/train.txt (list of image ids)
-    """
-    def __init__(self, root, image_set='train', transforms=None, classes=None):
+class COCODataset(Dataset):
+    def __init__(self, root, annotation_file):
         self.root = root
-        self.image_set = image_set
-        self.transforms = transforms
-        ids_file = os.path.join(root, 'ImageSets', 'Main', image_set + '.txt')
-        with open(ids_file) as f:
-            self.ids = [x.strip() for x in f.readlines()]
-        self.image_dir = os.path.join(root, 'JPEGImages')
-        self.ann_dir = os.path.join(root, 'Annotations')
-        # classes: list like ['__background__','car','person']
-        if classes is None:
-            self.classes = ['__background__','object']
-        else:
-            self.classes = classes
+        self.annotation_file = annotation_file
+
+        # Load COCO annotations
+        with open(annotation_file) as f:
+            data = json.load(f)
+
+        self.images = {img["id"]: img["file_name"] for img in data["images"]}
+        self.annotations = data["annotations"]
+
+        # Group annotations by image ID
+        self.img_to_anns = {}
+        for ann in self.annotations:
+            img_id = ann["image_id"]
+            if img_id not in self.img_to_anns:
+                self.img_to_anns[img_id] = []
+            self.img_to_anns[img_id].append(ann)
+
+        # Transform to convert PIL images to tensors
+        self.transform = transforms.Compose([transforms.ToTensor()])
 
     def __len__(self):
-        return len(self.ids)
+        return len(self.images)
 
     def __getitem__(self, idx):
-        img_id = self.ids[idx]
-        img_path = os.path.join(self.image_dir, img_id + '.jpg')
-        ann_path = os.path.join(self.ann_dir, img_id + '.xml')
-        img = Image.open(img_path).convert('RGB')
-        boxes = []
-        labels = []
-        tree = ET.parse(ann_path)
-        root = tree.getroot()
-        for obj in root.findall('object'):
-            name = obj.find('name').text
-            if name not in self.classes:
-                # map unknown classes to background (skip)
-                continue
-            labels.append(self.classes.index(name))
-            bndbox = obj.find('bndbox')
-            xmin = float(bndbox.find('xmin').text)
-            ymin = float(bndbox.find('ymin').text)
-            xmax = float(bndbox.find('xmax').text)
-            ymax = float(bndbox.find('ymax').text)
-            boxes.append([xmin, ymin, xmax, ymax])
-        boxes = torch.as_tensor(boxes, dtype=torch.float32)
-        labels = torch.as_tensor(labels, dtype=torch.int64)
-        target = {}
-        target['boxes'] = boxes
-        target['labels'] = labels
-        target['image_id'] = torch.tensor([idx])
-        if self.transforms is not None:
-            img, target = self.transforms(img, target)
+        image_id = list(self.images.keys())[idx]
+        img_path = os.path.join(self.root, self.images[image_id])
+        img = Image.open(img_path).convert("RGB")
+        img = self.transform(img)
+
+        # Load annotations for this image
+        anns = self.img_to_anns.get(image_id, [])
+
+        if len(anns) == 0:
+            # Skip images with no bounding boxes
+            boxes = torch.zeros((0, 4), dtype=torch.float32)
+            labels = torch.zeros((0,), dtype=torch.int64)
+        else:
+            boxes = []
+            labels = []
+            for ann in anns:
+                bbox = ann["bbox"]  # COCO format: [x, y, width, height]
+                x_min = bbox[0]
+                y_min = bbox[1]
+                x_max = bbox[0] + bbox[2]
+                y_max = bbox[1] + bbox[3]
+                boxes.append([x_min, y_min, x_max, y_max])
+                labels.append(ann["category_id"])
+
+            boxes = torch.as_tensor(boxes, dtype=torch.float32)
+            labels = torch.as_tensor(labels, dtype=torch.int64)
+
+        target = {
+            "boxes": boxes,
+            "labels": labels,
+            "image_id": torch.tensor([image_id])
+        }
+
         return img, target
